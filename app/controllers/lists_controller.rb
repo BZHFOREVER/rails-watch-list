2812f305a9340c15d70172d7f9837d2e3ef8589e
Gaetan RENAULT <gadetan@gmail.com>
2022-11-18 15:27:27 +0100

    show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,7 @@ class ListsController < ApplicationController
 
   def show
     @bookmark = Bookmark.new
-    @review = Review.new(list: @list)
+    @review = Review.new(list: @lists)
   end
 
   def new
